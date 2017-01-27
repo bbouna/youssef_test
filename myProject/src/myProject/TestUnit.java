4858e614b0abc89add77dc937526e1a584e5a490
Youssef <Youssef@UA-FG>
2017-01-27 11:20:27 +0200

    methode afficher

--- myProject/src/myProject/TestUnit.java
+++ myProject/src/myProject/TestUnit.java
@@ -4,4 +4,9 @@ public class TestUnit {
 public static void main (String args[]){
 	System.out.println("hello ");
 }
+public void afficher(){
+	
+	for(int i=0;i<10;i++)System.out.println("bonjour");
+	
+}
 }
