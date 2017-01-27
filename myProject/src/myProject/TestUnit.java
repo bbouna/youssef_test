5fa90533980ab3fad88ef40621ce5bbf67e57aff
Youssef <Youssef@UA-FG>
2017-01-27 11:26:41 +0200

    test

--- myProject/src/myProject/TestUnit.java
+++ myProject/src/myProject/TestUnit.java
@@ -3,6 +3,8 @@ package myProject;
 public class TestUnit {
 public static void main (String args[]){
 	System.out.println("hello ");
+	//Hello 
+	System.out.println("Sttt");
 }
 public void afficher(){
 	
